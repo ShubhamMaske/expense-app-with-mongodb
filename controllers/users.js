fdefc3cb0fd594ee8914a121bd88f1b5266d92e5
ShubhamMaske <shubhammaske099@gmail.com>
2023-03-16 10:49:28 +0530

    fifth commit passward encryption

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 
 const User = require('../models/users');
+const bcrypt = require('bcrypt');
 
 function isStringinValid(string){
     if(string == undefined || string.length === 0){
@@ -9,29 +10,26 @@ function isStringinValid(string){
     }
 }
 
+// ----------------- For SignUp -------------------
 exports.addUser = async (req, res, next)=> {
     try{
         const name = req.body.name;
         const email = req.body.email;
         const password = req.body.password;
 
-        const data = await User.create({
-            name: name,
-            email: email,
-            password: password
+        bcrypt.hash(password,10,async(err,hash) => {
+            const data = await User.create({name,email,password:hash})
+            console.log("after creating-->"+JSON.stringify(data));
+            res.status(201).json({newUser: data});
         })
-
-        console.log("after creating-->"+JSON.stringify(data));
-        res.status(201).json({newUser: data});
-
+        
     }
     catch(err){
-        res.status(500).json({
-            error: err
-        })
+        res.status(500).json({error: err})
     }
 }
 
+//----------------  for Sign In    -----------------
 exports.checkUser = async(req, res, next) =>{
     try{
         const {email, password} = req.body;
@@ -39,22 +37,25 @@ exports.checkUser = async(req, res, next) =>{
             res.status(400).json({message: " Bad Parameters", success:false})
         }
         console.log(password);
-        let result = await User.findAll({
-            where: {
-                email: `${email}`
-            }})
-        console.log(result);
-        console.log(result[0]);
-        //console.log(result[0].password);
-        if(result.length > 0){
-            if(result[0].password == password){
-                res.status(201).json({success:true, message: "User login Sucessful"});
-            }else{
-                res.status(401).json({success:false, message: "User not authorized"}) 
-            }    
+        let user = await User.findAll({where: {email}})
+        //console.log(result);
+        console.log(user[0]);
+        console.log(user[0].password);
+        if(user.length > 0){
+            bcrypt.compare(password,user[0].password, (err, result)=>{
+                if(err){
+                    throw new Error("Something went wrong");
+                }
+                if(result === true){
+                    res.status(201).json({success:true, message: "User login Sucessful"});  
+                }
+                else{
+                    return res.status(401).json({success:false, message: "User not authorized"}) 
+                }
+            })   
         }
         else{
-            res.status(404).json({success:false, message:"user Doesn't exit"});
+            return res.status(404).json({success:false, message:"user Doesn't exit"});
         }
 
     }
