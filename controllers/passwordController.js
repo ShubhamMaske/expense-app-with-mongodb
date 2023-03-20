7c6b218a2b220c813fa5c4777035ff54f611e9da
ShubhamMaske <shubhammaske099@gmail.com>
2023-03-21 00:58:00 +0530

    forgot password feature using Sendinblue

--- controllers/passwordController.js
+++ controllers/passwordController.js
@@ -1,13 +1,41 @@
 
-
-
+const Sib = require('sib-api-v3-sdk');
+require('dotenv').config()
 
 exports.forgotPassword = async(req, res, next)=>{
     try{
-        const email = req.body.email;
-        console.log("Your email is >>>> ",email);
+        const Email = req.body.email;
+        console.log("Your email is >>>> ",Email);
+        const client = Sib.ApiClient.instance
+        const apiKey = client.authentications['api-key']
+        apiKey.apiKey = process.env.API_KEY
+
+        const tranEmailApi = new Sib.TransactionalEmailsApi()
+        const sender = {
+        email: 'maske3317@gmail.com',
+        }
+        const receiver = [
+        {
+            email: Email,
+        },
+        ]
+
+        tranEmailApi
+    .sendTransacEmail({
+        sender,
+        to: receiver,
+        subject: 'Password Reset mail',
+        textContent: `
+        Click the link to reset password.
+        `,
+    })
+    .then((result)=>{
+        console.log(result)
+        return res.status(200).json({message: 'Link to reset password sent to your mail ', sucess: true})
+    })
     }
     catch(err){
-        console.log(err)
+        res.status(500).json({error:err});
+        console.log("Error---<>---",err)
     }
 }
--- views/forgotpassword.js
+++ views/forgotpassword.js
@@ -1,12 +1,21 @@
 
 document.getElementById('forgotpassword').onclick = async function(e){
-    e.preventDefault();
-    const email = document.getElementById('email').value;
-    document.getElementById('email').value = "";
+    try{
+        e.preventDefault();
+        const email = document.getElementById('email').value;
+        document.getElementById('email').value = "";
 
-    let Obj = {
-        email: email
+        let Obj = {
+            email: email
+        }
+
+        const result = await axios.post("http://localhost:3000/password/forgotpassword",Obj);
+        document.body.innerHTML += '<div style="color:red;">Mail Successfuly sent <div>'
+        console.log("result--->",result)
+
     }
-
-    const result = await axios.post("http://localhost:3000/password/forgotpassword",Obj);
+    catch(err){
+        console.log(err);
+        document.body.innerHTML += `<div style="color:red;">${err} <div>`;
+    }
 }
