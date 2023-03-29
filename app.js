205dffe5cf3b8dd6ba70b82ad084d8132cb9dc67
ShubhamMaske <shubhammaske099@gmail.com>
2023-03-29 11:01:28 +0530

    updation for cicd deployment

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'views')));
 
 const accessLogStream = fs.createWriteStream(
     path.join(__dirname, 'access.log'), 
